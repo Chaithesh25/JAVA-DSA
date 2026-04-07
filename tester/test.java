48532aa1ea593b18424685f3eec8e853cb24678a
chaithesh <chaithesh121@gmail.com>
2026-04-07 09:47:27 +0530

    tester file content changed

---
+++ tester/test.java
@@ -0,0 +1,14 @@
+package tester;
+import Bank.BankAc;
+
+
+public class test {
+    public static void main(String[] args) {
+        
+        BankAc obj = new BankAc();
+        obj.name = "chaithesh"; 
+        obj.display1();
+        System.out.println("this is inside of test.java");
+    }
+    
+}
